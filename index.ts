f5ec6d0e2cb859c7e26de2aa29e9667012306e76
SkullRex001 <honestaditya@gmail.com>
2025-12-07 17:39:08 +0530

    cors

--- index.ts
+++ index.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import  {getCustomers} from "./controller/dbquery.js";
+import cors from 'cors';
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.get('/health' , (req , res)=>{
     res.json({
